b148e5d1362cb3611eb6165ddea5140d6278f410
Foxes20 <63357161+Foxes20@users.noreply.github.com>
2023-04-21 15:39:16 +0700

    fixed adding category entries where parenet_id==null

--- cron/addEntries.php
+++ cron/addEntries.php
@@ -3,13 +3,21 @@
 define("CUR_DIR", __DIR__.'./../');
 
 require CUR_DIR . 'core/db.php';
+$db = new \core\db();
 
-    $db = new \core\db();
+for ($i = 0; $i <= 100; $i++) {
+    $rand = (rand(1, 99));
 
-    for ($i = 0; $i <= 5000; $i++) {
-        $rand = (rand(1, 99));
+    if ($i % $rand == 0) {
+
         $sql = "INSERT INTO `subcategories` (`parent_id`, `name`)
-                                                    VALUES ($rand, ('category{$i}'))";
+                                                    VALUES (NULL, 'category{$i}')";
+        mysqli_query($db->connect, $sql) or die (mysqli_error($db->connect));
 
+    } else {
+
+        $sql = "INSERT INTO `subcategories` (`parent_id`, `name`)
+                                                    VALUES ($rand, 'category{$i}')";
         mysqli_query($db->connect, $sql) or die (mysqli_error($db->connect));
     }
+}
